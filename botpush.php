355ec679c0fd6fec9bbc52d2e3472d9aa5be457c
wattim92 <50196385+wattim92@users.noreply.github.com>
2019-05-02 15:44:38 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,9 @@
 
 require "vendor/autoload.php";
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '6D+01eig436Njakvxmrdvc98eZ+x9S56W7p/J9m61gghqCSJ9ilqy4QZumarStF0qETOeBoD9b99M1Y6OPzqUQvc4r+lgJ+7br+zxmZlaMgTAXkACsERtqYLx9Yvwc+2QEOHumzlFwOlGlDZEZyQ0QdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '75c03f392f6e53d662d6f5a8db9e421f';
+$channelSecret = 'a7509844466aa2de2f10fb88a6a18c08';
 
 $pushID = 'U7ef7a449f2a5c2057eacfc02ba2eb286';
 
